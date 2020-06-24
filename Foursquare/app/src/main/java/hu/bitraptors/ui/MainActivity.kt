6d735ec73e1c104a378329cfb356538f8877cbd6
Hajas András <bandi.14@hotmail.com>
2020-06-24 15:27:52 +0200

    Adding map activity

--- Foursquare/app/src/main/java/hu/bitraptors/ui/MainActivity.kt
+++ Foursquare/app/src/main/java/hu/bitraptors/ui/MainActivity.kt
@@ -1,27 +1,38 @@
 package hu.bitraptors.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import hu.bitraptors.R
 import hu.bitraptors.model.details.DetailsResponse
 import hu.bitraptors.model.search.SearchResponse
+import hu.bitraptors.model.search.Venue
 import hu.bitraptors.retrofit.RetrofitClient
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var nearVenues : List<Venue>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btToMap.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
 
         RetrofitClient.venueService.findVenues().enqueue(object : RetrofitClient.VenueCallback<SearchResponse> {
             override fun onResponse(call: Call<SearchResponse>, response: Response<SearchResponse>) {
                 Log.d("retrofit", response.code().toString())
                 if(response.isSuccessful){
                     Log.d("retrofit", response.body()!!.response!!.venues?.get(0)?.name!!)
+
                 }
             }
         })
